71f9e848066d717b259b761419cc1c0a18c4cbbf
rudra2955 <rudramishra29055@gmail.com>
2025-09-21 06:13:06 +0530

    Time: 4 ms (69.81%) | Memory: 45.2 MB (14.40%) - LeetSync

---
+++ Stacks_and_Queue/13-roman-to-integer/roman-to-integer.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int romanToInt(String s) {
+        Map<Character, Integer> roman = new HashMap<>();
+        roman.put('I', 1);
+        roman.put('V', 5);
+        roman.put('X', 10);
+        roman.put('L', 50);
+        roman.put('C', 100);
+        roman.put('D', 500);
+        roman.put('M', 1000);
+
+        int total = 0;
+        int prev = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {  // right to left
+            int curr = roman.get(s.charAt(i));
+            if (curr < prev) {
+                total -= curr;
+            } else {
+                total += curr;
+            }
+            prev = curr;
+        }
+        return total;
+    }
+}
