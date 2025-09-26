779225dc98531068132714e38d491853ee6423b9
rudra2955 <rudramishra29055@gmail.com>
2025-09-26 14:28:41 +0530

    Time: 23 ms (85.21%) | Memory: 43.8 MB (10.11%) - LeetSync

--- Stacks_and_Queue/611-valid-triangle-number/valid-triangle-number.java
+++ Stacks_and_Queue/611-valid-triangle-number/valid-triangle-number.java
@@ -1,16 +1,17 @@
 class Solution {
     public int triangleNumber(int[] nums) {
         Arrays.sort(nums);
+        int n = nums.length;
         int count = 0;
-        for (int i = 0; i < nums.length - 2; i++) {
-            for (int j = i + 1; j < nums.length - 1; j++) {
-                for (int k = j + 1; k < nums.length; k++) {
-                    int a = nums[i];
-                    int b = nums[j];
-                    int c = nums[k];
-                    if (a + b > c && a + c > b && b + c > a) {
-                        count++;
-                    }
+
+        for (int i = n - 1; i >= 2; i--) {
+            int left = 0, right = i - 1;
+            while (left < right) {
+                if (nums[left] + nums[right] > nums[i]) {
+                    count += right - left;
+                    right--;
+                } else {
+                    left++;
                 }
             }
         }
